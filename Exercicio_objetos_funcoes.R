38a7143824a53d3dfe4d8baaa2167fd2846a2abb
Pedro Henrique <pedrohenrique.silva@ufpe.br>
2021-04-12 21:39:10 -0300

    Código referente à atividade "Objetos e Funções"

---
+++ Exercicio_objetos_funcoes.R
@@ -0,0 +1,15 @@
+#1 - objeto simples
+#Objeto: vetor
+vector <- c(1,2,3,4,5)
+
+#Complexidade do objeto
+str(vector)
+
+
+#2 - Objeto complexo
+#Objeto: regressão linear
+iris <- lm(Sepal.Length ~ Sepal.Width,iris)
+
+#Complexidade do objeto
+str(iris)
+
