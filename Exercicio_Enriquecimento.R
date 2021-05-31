7561f27dbfc9de16f85bcd2370b07f5351295aec
PHRS82 <82379872+PHRS82@users.noreply.github.com>
2021-05-31 00:46:53 -0300

    Correção do exercício sobre enriquecimento

--- Exercicio_Enriquecimento.R
+++ Exercicio_Enriquecimento.R
@@ -1,24 +1,15 @@
 library(poliscidata)
 
-
-campusIES <- left_join(campusIES, baseCodMun, by = c('CO_MUNICIPIO' = 'id_munic_7'))
-
-
-
-
 df <- world
-
 latin_america_paises<-c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela")
-
 dflatin <- df %>% filter(country %in% latin_america_paises)
-
 dflatinok <- dflatin %>% filter(gini10>=0)
+
+
 
 #juntar tabela que contém informações do índice gini10 com a do IDH
 hdi <- read.csv2('bases_originais/hdi-ok.csv')
-
 hdilatin <- hdi %>% 
   filter(country %in% latin_america_paises)
-
 
 idhginilatin <- left_join(dflatinok, hdilatin, by = c('country' = 'country'))
