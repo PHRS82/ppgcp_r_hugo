b596ad88d9e884d7a95079f0931332062108499f
PHRS82 <82379872+PHRS82@users.noreply.github.com>
2021-06-21 22:42:41 -0300

    Exercícios - Juntando e Buscando

--- Exercicio_Juntando_Buscando.R
+++ Exercicio_Juntando_Buscando.R
@@ -2,8 +2,8 @@ library(fuzzyjoin)
 library(dplyr)
 library(tidyr)
 
-baseA <- read.csv2('bases_originais/Serie.csv')
-baseB <- read.csv2('bases_originais/Estado.csv') 
+baseA <- read.csv2('bases_tratadas/Serie.csv')
+baseB <- read.csv2('bases_tratadas/Estado.csv') 
 baseC <- fuzzyjoin::stringdist_join(baseA, baseB, mode='left')
 
 
